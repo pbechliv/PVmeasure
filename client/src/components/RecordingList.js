74edd3077ff2e09fd1253b9f604e818cf92d10a9
Panos Bechlivanos <panagiotis.bechlivanos@earthanalytics.no>
2019-03-23 12:54:57 +0200

    dockerfiles and compose files need for production

---
+++ client/src/components/RecordingList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Card, Icon } from "semantic-ui-react";
+
+const RecordingsList = props => {
+  return 
+};
+
+export default RecordingsList;
--- client/src/containers/RecordingsListPage.js
+++ client/src/containers/RecordingsListPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Grid } from "semantic-ui-react";
+import { Grid, Icon, Table } from "semantic-ui-react";
+import { toastr } from "react-redux-toastr";
 import RecordingForm from "../components/RecordingForm";
 import { setFetchHeaders } from "../lib";
 import { HOST_URL } from "..";
@@ -44,13 +45,75 @@ class RecordingsListPage extends React.Component {
     }
   }
 
+  async deleteRecording(recording) {
+    toastr.confirm(`Are you sure you want to delete this recording?`, {
+      onOk: async () => {
+        const headers = setFetchHeaders("DELETE");
+        try {
+          const response = await fetch(
+            `${HOST_URL}/measurement_groups/${recording.id}/`,
+            headers
+          );
+          if (response.ok) {
+            toastr.warning(`The recording has been successfully deleted...`);
+            this.props.removeGroup(recording);
+          }
+        } catch (e) {
+          console.log(e);
+          toastr.error(`Something went wrong...`);
+        }
+      }
+    });
+  }
   render() {
     return (
       <Grid stackable>
         <Grid.Column width={10}>
           <RecordingForm />
         </Grid.Column>
-        <Grid.Column width={6}>List</Grid.Column>
+        <Grid.Column width={6}>
+          {this.props.recordings.results.map((recording, index) => (
+            <Table textAlign="left" key={`recording-${index}`}>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>Spot</Table.HeaderCell>
+                  <Table.HeaderCell>Measurements</Table.HeaderCell>
+                  <Table.HeaderCell>Comment</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                <Table.Row>
+                  <Table.Cell>{recording.measuring_point}</Table.Cell>
+                  <Table.Cell>
+                    {recording.measurements.map((m, i) => (
+                      <div key={`mr-${i}`}>
+                        <strong>{m.type}:</strong>&nbsp;
+                        {m.value} {m.unit}
+                      </div>
+                    ))}
+                    {recording.polarity_test && (
+                      <div>
+                        <strong>Polarity</strong>&nbsp;
+                        <Icon name="check" />
+                      </div>
+                    )}
+                  </Table.Cell>
+                  <Table.Cell>{recording.comment}</Table.Cell>
+                </Table.Row>
+                {/* <Table.Row>
+                  <Table.Cell>
+                    {recording.measurements.map((m, i) => (
+                      <div key={`mr-${i}`}>{m.type}</div>
+                    ))}
+                  </Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>{recording.comment}</Table.Cell>
+                </Table.Row> */}
+              </Table.Body>
+            </Table>
+          ))}
+        </Grid.Column>
       </Grid>
     );
   }
@@ -62,7 +125,8 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = {
   setCurrentGroup: actions.setCurrentGroup,
-  setRecordings: actions.setRecordings
+  setRecordings: actions.setRecordings,
+  removeRecording: actions.removeRecording
 };
 
 export default connect(
